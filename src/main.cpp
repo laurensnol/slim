128a6f7e17244dce2af2c1a97a8c121a8fb1798e
Laurens Nolting <contact@laurensnol.com>
2023-03-15 05:37:20 +0100

    Implemented glad in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
+#include <stdio.h>
 
-#include "glfw/glfw3.h"
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
 
 int main()
 {
@@ -8,6 +9,14 @@ int main()
 
   if (!glfwInit())
     return -1;
+
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  #if SLIM_PLATFORM_MACOS
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  #endif
 
   window = glfwCreateWindow(1080, 720, "slim", NULL, NULL);
   
@@ -18,7 +27,16 @@ int main()
   }
 
   glfwMakeContextCurrent(window);
-  
+
+  int version = gladLoadGL(glfwGetProcAddress);
+  if (version == 0)
+  {
+    printf("Failed to initialize OpenGL context\n");
+    return -1;
+  }
+
+  printf("Loaded OpenGL %d.%d\n", GLAD_VERSION_MAJOR(version), GLAD_VERSION_MINOR(version));
+
   while (!glfwWindowShouldClose(window))
   {
     glClear(GL_COLOR_BUFFER_BIT);

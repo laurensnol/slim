40c59bf82250c24aef129fbea6fdfa63c9e5121c
Laurens Nolting <contact@laurensnol.com>
2023-03-17 02:53:20 +0100

    Updated main.cpp to use Window class

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,24 @@
-#include <glad/gl.h>
-#include <GLFW/glfw3.h>
-#include <spdlog/spdlog.h>
+#include "core/window.h"
 
+#include <glad/gl.h>
 #include <imgui.h>
 #include <backends/imgui_impl_glfw.h>
 #include <backends/imgui_impl_opengl3.h>
+#include <memory>
 
 int main()
 {
-  GLFWwindow *window;
-
-  if (!glfwInit())
-    return -1;
-
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-  #ifdef SLIM_PLATFORM_MACOS
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-  #endif
-
-  window = glfwCreateWindow(1080, 720, "slim", NULL, NULL);
-  
-  if (!window)
-  {
-    glfwTerminate();
-    return -1;
-  }
-
-  glfwMakeContextCurrent(window);
-
-  int version = gladLoadGL(glfwGetProcAddress);
-  if (version == 0)
-  {
-    spdlog::critical("Failed to initialize OpenGL context");
-    return -1;
-  }
-
-  spdlog::info("Loaded OpenGL {}.{}", GLAD_VERSION_MAJOR(version), GLAD_VERSION_MINOR(version));
+  std::unique_ptr<slim::Window> window = slim::Window::create();
 
   IMGUI_CHECKVERSION();
   ImGui::CreateContext();
 
   ImGui::StyleColorsDark();
 
-  ImGui_ImplGlfw_InitForOpenGL(window, true);
+  ImGui_ImplGlfw_InitForOpenGL((GLFWwindow*)window->getNative(), true);
   ImGui_ImplOpenGL3_Init("#version 410");
 
-  while (!glfwWindowShouldClose(window))
+  while (!window->shouldClose())
   {
     glClear(GL_COLOR_BUFFER_BIT);
 
@@ -61,10 +31,8 @@ int main()
     ImGui::Render();
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-    glfwSwapBuffers(window);
-    glfwPollEvents();
+    window->update();
   }
 
-  glfwTerminate();
   return 0;
 }

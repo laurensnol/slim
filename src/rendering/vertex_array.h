1665226f446ad4e5a628fff3e279213a9028ee3f
Laurens Nolting <contact@laurensnol.com>
2023-03-23 21:28:03 +0100

    Created VertexArray class

---
+++ src/rendering/vertex_array.h
@@ -0,0 +1,28 @@
+#ifndef SLIM_VERTEXARRAY_H
+#define SLIM_VERTEXARRAY_H
+
+#include "rendering/vertex_buffer.h"
+#include "rendering/index_buffer.h"
+#include <memory>
+#include <vector>
+
+namespace slim
+{
+  class VertexArray
+  {
+  public:
+    virtual ~VertexArray() = default;
+
+    virtual void bind() = 0;
+    virtual void unbind() = 0;
+    
+    virtual void addVertexBuffer(const std::shared_ptr<VertexBuffer>& buffer) = 0;
+    virtual const std::vector<std::shared_ptr<VertexBuffer>>& getVertexBuffers() const = 0;
+
+    virtual void setIndexBuffer(const std::shared_ptr<IndexBuffer> &buffer) = 0;
+
+    static std::shared_ptr<VertexArray> create();
+  };
+}
+
+#endif

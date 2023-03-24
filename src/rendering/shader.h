7c87f205cded0159047b82cc30dc44f515ab144d
Laurens Nolting <contact@laurensnol.com>
2023-03-24 16:44:54 +0100

    Updated Shader class to implement uniform methods

--- src/rendering/shader.h
+++ src/rendering/shader.h
@@ -1,8 +1,10 @@
 #ifndef SLIM_PLATFORM_SHADER_H
 #define SLIM_PLATFORM_SHADER_H
 
-#include <memory>
+#include <string>
 #include <string_view>
+#include <glm/glm.hpp>
+#include <memory>
 
 namespace slim
 {
@@ -13,6 +15,12 @@ namespace slim
 
     virtual void bind() = 0;
     virtual void unbind() = 0;
+
+    virtual void setFloat(const std::string &name, float value) = 0;
+    virtual void setFloat2(const std::string &name, const glm::vec2 &value) = 0;
+    virtual void setFloat3(const std::string &name, const glm::vec3 &value) = 0;
+    virtual void setFloat4(const std::string &name, const glm::vec4 &value) = 0;
+    virtual void setInt(const std::string &name, uint32_t value) = 0;
 
     static std::unique_ptr<Shader> create(std::string_view vertexPath, std::string_view fragmentPath);
   };

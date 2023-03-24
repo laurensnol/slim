8db97d6d4bac499c83d73b8047a04099cc61f404
Laurens Nolting <contact@laurensnol.com>
2023-03-24 03:21:57 +0100

    Updated VertexBuffer
    - Added methods to add and get VertexAttributes

--- src/rendering/vertex_buffer.h
+++ src/rendering/vertex_buffer.h
@@ -1,6 +1,8 @@
 #ifndef SLIM_VERTEX_BUFFER_H
 #define SLIM_VERTEX_BUFFER_H
 
+#include "rendering/vertex_attribute.h"
+#include <vector>
 #include <memory>
 
 namespace slim
@@ -13,6 +15,9 @@ namespace slim
     virtual void bind() = 0;
     virtual void unbind() = 0;
     virtual void set(const void *data, uint32_t size) = 0;
+
+    virtual void addAttribute(const VertexAttribute &attribute) = 0;
+    virtual const std::vector<VertexAttribute> &getAttributes() const = 0;
 
     static std::shared_ptr<VertexBuffer> create(float *vertices, uint32_t size);
   };

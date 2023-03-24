93b8d4bf83ead0a75678ef0c3ee72a2ef93fb34b
Laurens Nolting <contact@laurensnol.com>
2023-03-24 18:23:39 +0100

    Created base.h

---
+++ src/core/base.h
@@ -0,0 +1,14 @@
+#ifndef SLIM_BASE_H
+#define SLIM_BASE_H
+
+#include <spdlog/spdlog.h>
+#include <assert.h>
+
+#define SLIM_ASSERT(value, msg, ...)      \
+  if (!value)                             \
+  {                                       \
+    spdlog::critical(msg, ##__VA_ARGS__); \
+    assert(value);                        \
+  }
+
+#endif

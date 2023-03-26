885cbe298367fb26d7febbcb060e93294b46a9d2
Laurens Nolting <contact@laurensnol.com>
2023-03-26 04:54:41 +0200

    Updated Vertex Shader and main.cpp
    - Now displays a colored 3D cube

--- res/vertex.glsl
+++ res/vertex.glsl
@@ -1,12 +1,16 @@
 #version 330 core
 
 layout (location = 0) in vec3 pos;
-layout (location = 1) in vec4 vertexColor;
+layout (location = 1) in vec3 vertexColor;
 
 out vec4 color;
 
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
 void main()
 {
-  gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);
-  color = vertexColor;
+  gl_Position = projection * view * model * vec4(pos, 1.0);
+  color = vec4(vertexColor, 1.0);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include "rendering/index_buffer.h"
 
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 #include <glad/gl.h>
 #include <imgui.h>
 #include <backends/imgui_impl_glfw.h>
@@ -27,57 +28,76 @@ int main()
   std::unique_ptr<slim::Shader> shader = slim::Shader::create("res/vertex.glsl", "res/fragment.glsl");
 
   float vertices[] = {
-    -0.5, -0.5, 0.0,  // SW
-     0.5, -0.5, 0.0,  // SE
-     0.5,  0.5, 0.0,  // NE
-    -0.5,  0.5, 0.0,  // NW
+    -0.5, -0.5, -0.5,   0.0, 0.0, 0.0, // Back Bottom Left
+     0.5, -0.5, -0.5,   0.0, 1.0, 0.0, // Back Bottom Right
+     0.5,  0.5, -0.5,   0.0, 1.0, 1.0, // Back Top Right
+    -0.5,  0.5, -0.5,   0.0, 0.0, 1.0, // Back Top Left
+    -0.5, -0.5,  0.5,   1.0, 0.0, 0.0, // Front Bottom Left
+     0.5, -0.5,  0.5,   1.0, 1.0, 0.0, // Front Bottom Right
+     0.5,  0.5,  0.5,   1.0, 1.0, 1.0, // Front Top Right
+    -0.5,  0.5,  0.5,   1.0, 0.0, 1.0  // Front Top Left
   };
 
-  float colors[] = {
-    0.0, 0.0, 0.0, 1.0, // SW
-    0.0, 0.0, 1.0, 1.0, // SE
-    0.0, 1.0, 1.0, 1.0, // NE
-    0.0, 1.0, 0.0, 1.0  // NW
-  };
-
-  unsigned int indices[] = {
-    0, 1, 3,
-    1, 2, 3
+  uint32_t indexCount = 36;
+  uint32_t indices[] = {
+    0, 1, 3, 3, 1, 2,
+    1, 5, 2, 2, 5, 6,
+    5, 4, 6, 6, 4, 7,
+    4, 0, 7, 7, 0, 3,
+    3, 2, 7, 7, 2, 6,
+    4, 5, 0, 0, 5, 1
   };
 
   std::shared_ptr<slim::VertexBuffer> vbo = slim::VertexBuffer::create(vertices, sizeof(vertices));
   
-  slim::VertexAttribute attr = slim::VertexAttribute(0, slim::VertexAttributeBaseType::Float3, false);
-  vbo->addAttribute(attr);
+  slim::VertexAttribute vertexAttr = slim::VertexAttribute(0, slim::VertexAttributeBaseType::Float3, false, 6);
+  vbo->addAttribute(vertexAttr);
 
-  std::shared_ptr<slim::VertexBuffer> colors_vbo = slim::VertexBuffer::create(colors, sizeof(colors));
-
-  slim::VertexAttribute colorAttr = slim::VertexAttribute(1, slim::VertexAttributeBaseType::Float4, false);
-  colors_vbo->addAttribute(colorAttr);
+  slim::VertexAttribute colorAttr = slim::VertexAttribute(1, slim::VertexAttributeBaseType::Float3, false, 6, 3 * sizeof(float));
+  vbo->addAttribute(colorAttr);
 
   std::shared_ptr<slim::VertexArray> vao = slim::VertexArray::create();
   vao->addVertexBuffer(vbo);
-  vao->addVertexBuffer(colors_vbo);
 
-  std::shared_ptr<slim::IndexBuffer> ibo = slim::IndexBuffer::create(indices, 6);
+  std::shared_ptr<slim::IndexBuffer> ibo = slim::IndexBuffer::create(indices, indexCount);
   vao->setIndexBuffer(ibo);
 
-  uint32_t indicesCount = ibo->count();
+  // Matrices
+  glm::mat4 model(1.0f);
+  model = glm::rotate(model, glm::radians(30.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+
+  glm::mat4 view(1.0f);
+  view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+
+  glm::vec2 dimensions = window->getDimensions();
+  glm::mat4 projection = glm::perspective(glm::radians(65.0f), dimensions.x / dimensions.y, 0.1f, 100.0f);
+
+  shader->bind();
+  shader->setMat4("model", model);
+  shader->setMat4("view", view);
+  shader->setMat4("projection", projection);
+
+  // Drawing
+  glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+  glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+  //glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
   while (!window->shouldClose())
   {
-    glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     ImGui_ImplOpenGL3_NewFrame();
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
 
-    shader->bind();
-    vao->bind();
-    glDrawElements(GL_TRIANGLES, indicesCount, GL_UNSIGNED_INT, 0);
+    ImGui::ShowMetricsWindow();
 
     ImGui::Render();
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    
+    shader->bind();
+    vao->bind();
+    glDrawElements(GL_TRIANGLES, indexCount, GL_UNSIGNED_INT, 0);
 
     window->update();
   }

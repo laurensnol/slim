1f5127fec957f9163626383fda9358c2aab7548b
Laurens Nolting <contact@laurensnol.com>
2023-03-29 19:09:23 +0200

    Created an Image utility class

---
+++ src/utils/image.cpp
@@ -0,0 +1,26 @@
+#include "utils/image.h"
+#include "core/base.h"
+#include <stb_image.h>
+
+namespace slim
+{
+  Image::Image(_ImageKey, const std::string &path)
+  {
+    data = stbi_load(path.c_str(), &width, &height, &channels, 0);
+
+    if (!data)
+      success = false;
+    else
+      success = true;
+  }
+
+  Image::~Image()
+  {
+    stbi_image_free(data);
+  }
+
+  std::shared_ptr<Image> Image::load(const std::string &path)
+  {
+    return std::make_shared<Image>(_ImageKey{}, path);
+  }
+}
---
+++ src/utils/image.h
@@ -0,0 +1,35 @@
+#ifndef SLIM_IMAGE_H
+#define SLIM_IMAGE_H
+
+#include <memory>
+#include <string>
+
+namespace slim
+{
+  class Image
+  {
+    // Passkey idiom to restrict initializing Image directly while still allowing
+    // std::make_shared.
+    // See: https://abseil.io/tips/134
+    class _ImageKey
+    {
+    private:
+      explicit _ImageKey() = default;
+      friend Image;
+    };
+
+  public:
+    Image(_ImageKey, const std::string &path);
+    virtual ~Image();
+
+    int32_t width;
+    int32_t height;
+    int32_t channels;
+    unsigned char *data;
+    bool success;
+
+    static std::shared_ptr<Image> load(const std::string &path);
+  };
+}
+
+#endif

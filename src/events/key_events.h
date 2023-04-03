4cef3ff1d194c0c5f9207ed106dcf773d7e15be4
Laurens Nolting <contact@laurensnol.com>
2023-04-03 03:35:44 +0200

    Created KeyDownEvent and KeyUpEvent

---
+++ src/events/key_events.h
@@ -0,0 +1,39 @@
+#ifndef SLIM_KEYEVENTS_H
+#define SLIM_KEYEVENTS_H
+
+#include "events/event.h"
+#include "events/codes.h"
+#include <cstdint>
+
+namespace slim
+{
+  /**
+   * KeyDownEvent
+   */
+  class KeyDownEvent : public Event
+  {
+  public:
+    KeyDownEvent(KeyCode keyCode, uint8_t modifiers);
+    
+    EventType getType() const override;
+
+    KeyCode keyCode;
+    uint8_t modifiers;
+  };
+
+  /**
+   * KeyUpEvent
+   */
+  class KeyUpEvent : public Event
+  {
+  public:
+    KeyUpEvent(KeyCode keyCode, uint8_t modifiers);
+    
+    EventType getType() const override;
+
+    KeyCode keyCode;
+    uint8_t modifiers;
+  };
+}
+
+#endif

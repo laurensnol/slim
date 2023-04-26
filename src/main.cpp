261b65e0c7c027a16695457a216f5b67dabfe164
Laurens Nolting <contact@laurensnol.com>
2023-04-26 18:23:58 +0200

    Updated `main.cpp`
    - Now uses `slim::SceneManager` and `slim::DemoScene`

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
+#include "demo_scene.h"
+#include "scene/scene_manager.h"
 #include "core/application.h"
 
 int main()
-{  
+{
+  slim::SceneManager::addScene<slim::DemoScene>("demo");
+
   slim::Application& app = slim::Application::getInstance();
   app.start();
 

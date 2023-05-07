72d1365e7e3c01615788a5f7e026bf985700d683
Laurens Nolting <contact@laurensnol.com>
2023-05-08 01:54:23 +0200

    Added specular lighting and variable parameters
    - Updated `flat.frag` to take in the view position as well as other parameters as uniforms
    - Updated `DemoScene` to set `uViewPosition` and other parameters
    - Updated UI

--- res/flat.frag
+++ res/flat.frag
@@ -6,14 +6,17 @@ in vec3 oFragPos;
 uniform vec3 uColor;
 uniform vec3 uLightColor;
 uniform vec3 uLightPosition;
+uniform vec3 uViewPosition;
+uniform float uAmbientStrength;
+uniform float uSpecularStrength;
+uniform int uShininess;
 
 out vec4 oFragColor;
 
 void main()
 {
   // Ambient
-  float ambientStrength = 0.1;
-  vec3 ambient = ambientStrength * uLightColor;
+  vec3 ambient = uAmbientStrength * uLightColor;
 
   // Diffuse
   vec3 norm = normalize(oNormal);
@@ -21,6 +24,12 @@ void main()
   float diff = max(dot(norm, lightDirection), 0.0);
   vec3 diffuse = diff * uLightColor;
 
-  vec3 result = (ambient + diffuse) * uColor;
+  // Specular
+  vec3 viewDirection = normalize(uViewPosition - oFragPos);
+  vec3 reflectDirection = reflect(-lightDirection, norm);
+  float spec = pow(max(dot(viewDirection, reflectDirection), 0.0), uShininess);
+  vec3 specular = uSpecularStrength * spec * uLightColor;
+
+  vec3 result = (ambient + diffuse + specular) * uColor;
   oFragColor = vec4(result, 1.0);
 }
--- src/demo_scene.cpp
+++ src/demo_scene.cpp
@@ -60,6 +60,9 @@ namespace slim
     glm::vec3( 1.5f,  4.0f, -1.5f)
   };
 
+  const char* DemoScene::s_shininessLabels[] = {"2", "4", "8", "16", "32", "64", "128", "256", "512"};
+  const int DemoScene::s_shininessValues[] = {2, 4, 8, 16, 32, 64, 128, 256, 512};
+
   void DemoScene::onAttach()
   {
     std::shared_ptr<VertexBuffer> vbo = VertexBuffer::create(s_vertices, sizeof(s_vertices));
@@ -95,12 +98,17 @@ namespace slim
     m_cameraYaw = m_camera.getYaw();
     m_cameraFov = m_camera.getFov();
 
+    // Update Cube Shader
     m_cubeShader->bind();
     m_cubeShader->setMat4("uView", m_camera.getView());
     m_cubeShader->setMat4("uProjection", m_camera.getProjection());
     m_cubeShader->setFloat3("uColor", {1.0f, 0.0f, 0.0f});
     m_cubeShader->setFloat3("uLightColor", {1.0f, 1.0f, 1.0f});
     m_cubeShader->setFloat3("uLightPosition", m_lightPosition);
+    m_cubeShader->setFloat3("uViewPosition", m_camera.getPosition());
+    m_cubeShader->setFloat("uAmbientStrength", m_ambientStrength);
+    m_cubeShader->setFloat("uSpecularStrength", m_specularStrength);
+    m_cubeShader->setInt("uShininess", s_shininessValues[m_shininessIndex]);
     m_cubeVao->bind();
 
     for (auto&& position : s_cubePositions)
@@ -112,6 +120,7 @@ namespace slim
       glDrawArrays(GL_TRIANGLES, 0, s_indicesCount);
     }
 
+    // Update Light Shader
     m_lightShader->bind();
     m_lightShader->setMat4("uView", m_camera.getView());
     m_lightShader->setMat4("uProjection", m_camera.getProjection());
@@ -152,6 +161,12 @@ namespace slim
       lightModel = glm::scale(lightModel, {0.25f, 0.25f, 0.25f});
       m_lightShader->setMat4("uModel", lightModel);
     }
+
+    ImGui::SliderFloat("Ambient", &m_ambientStrength, 0.0f, 1.0f);
+
+    ImGui::Text("Shader");
+    ImGui::SliderFloat("Specular", &m_specularStrength, 0.0f, 1.0f);
+    ImGui::Combo("Shininess", &m_shininessIndex, s_shininessLabels, IM_ARRAYSIZE(s_shininessLabels));
 
     ImGui::End();
 

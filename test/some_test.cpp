005ad3e01111496f4f25c48cb95a45da2c86a0af
Laurens Nolting <contact@laurensnol.com>
2024-03-09 14:46:15 +0100

    Removed some_test.cpp

--- test/some_test.cpp
+++
@@ -1,7 +0,0 @@
-#define CATCH_CONFIG_MAIN
-
-#include <catch2/catch_test_macros.hpp>
-#include <slim/some.hpp>
-
-TEST_CASE("Correct", "[add]") { REQUIRE(slim::add(1, 2) == 3); }
-TEST_CASE("Incorrect", "[add]") { REQUIRE_FALSE(slim::add(1, 2) == 4); }

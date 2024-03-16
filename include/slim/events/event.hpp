8eddfbd95fe4bcb7047e3045b0689a2d91e3e4a5
Laurens Nolting <contact@laurensnol.com>
2024-03-16 01:13:17 +0100

    Created Event class

---
+++ include/slim/events/event.hpp
@@ -0,0 +1,22 @@
+#ifndef SLIM_EVENTS_EVENT_HPP_
+#define SLIM_EVENTS_EVENT_HPP_
+
+#define SLIM_DEFINE_EVENT(name)       \
+  static constexpr Type type = #name; \
+  [[nodiscard]] auto getType() const noexcept -> Type override { return type; }
+
+namespace slim {
+class Event {  // NOLINT(cppcoreguidelines-special-member-functions): No need
+protected:
+  using Type = const char *;
+
+  Event() = default;
+
+public:
+  virtual ~Event() = default;
+
+  [[nodiscard]] virtual auto getType() const noexcept -> Type = 0;
+};
+}  // namespace slim
+
+#endif  // SLIM_EVENTS_EVENT_HPP_

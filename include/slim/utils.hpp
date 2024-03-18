6efc1633ebab4e7b5e564a718ec056bfa0ea59e5
Laurens Nolting <contact@laurensnol.com>
2024-03-19 00:46:22 +0100

    Created MouseButton and Key enums

---
+++ include/slim/utils.hpp
@@ -0,0 +1,14 @@
+#ifndef SLIM_UTILS_HPP_
+#define SLIM_UTILS_HPP_
+
+#include <type_traits>
+
+// Convert enum class to it's underlying type (integral promotion)
+template <typename EnumType>
+constexpr auto operator+(EnumType value) noexcept
+    -> std::enable_if_t<std::is_enum_v<EnumType>,
+                        std::underlying_type_t<EnumType>> {
+  return static_cast<std::underlying_type_t<EnumType>>(value);
+}
+
+#endif  // SLIM_UTILS_HPP_

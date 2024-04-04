2d2622e7e71cc3952718a2de61acfd37297463bf
Laurens Nolting <contact@laurensnol.com>
2024-04-05 01:46:23 +0200

    Inlined utils::readFile

--- include/slim/utils.hpp
+++ include/slim/utils.hpp
@@ -2,7 +2,14 @@
 #define SLIM_UTILS_HPP_
 
 // IWYU pragma: no_include <__fwd/string_view.h>
+// IWYU pragma: no_include <__fwd/ios.h>
+// IWYU pragma: no_include <__fwd/fstream.h>
+// IWYU pragma: no_include <filesystem>
 
+#include <cassert>
+#include <cstddef>
+#include <fstream>
+#include <ios>  // IWYU pragma: keep
 #include <string>
 #include <string_view>  // IWYU pragma: keep
 #include <type_traits>
@@ -16,7 +23,21 @@ constexpr auto operator+(EnumType value) noexcept
 }
 
 namespace slim::utils {
-auto readFile(std::string_view path) noexcept -> std::string;
+inline auto readFile(std::string_view path) noexcept -> std::string {
+  auto file = std::ifstream(path, std::ios::in);
+  assert(file);
+
+  file.seekg(0, std::ios::end);
+  const auto size = file.tellg();
+  assert(size != 0);
+
+  auto buf = std::string(static_cast<size_t>(size), ' ');
+
+  file.seekg(0);
+  file.read(&buf[0], size);  // NOLINT
+
+  return buf;
 }
+}  // namespace slim::utils
 
 #endif  // SLIM_UTILS_HPP_
